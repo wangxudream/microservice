8320b99373c56487c3558af8f2869e57de3c08e9
wangxudream <2528300657@qq.com>
2020-09-16 17:34:39 +0800

    修改pom依赖

--- goods/src/main/java/com/kataer/microservice/goods/GoodsApplication.java
+++ goods/src/main/java/com/kataer/microservice/goods/GoodsApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class GoodsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
     }
