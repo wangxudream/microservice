55102327664d6a26a5830bfd29137e52e2ee9bdb
wangxudream <2528300657@qq.com>
2020-09-16 13:06:20 +0800

    spring微服务学习第一次提交

--- goods/src/main/java/com/kataer/microservice/goods/GoodsApplication.java
+++ goods/src/main/java/com/kataer/microservice/goods/GoodsApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class GoodsApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
     }
